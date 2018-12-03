23c9d789dd19992b8e6496a8d605dfc0b9997d86
mooncinnamon <moon.pinnamon@gmail.com>
2018-12-04 06:50:35 +0900

    make ejs sidebar

--- router/main.js
+++ router/main.js
@@ -3,7 +3,19 @@ module.exports = function(app, fs)
     app.get('/',function(req,res){
         res.render('index', {
             title: "Artik Boiler",
-            mainTitle: "House Boiler"
+            mainTitle: "House Boiler",
+            dashboad: true,
+            config : false
         })
     });
+
+    app.get('/config', function (req,res) {
+        res.render('config',{
+            title: "Artik Boiler",
+            mainTitle: "House Boiler",
+            dashboad: false,
+            config : true
+        })
+
+    })
 }
